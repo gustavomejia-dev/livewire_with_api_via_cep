83ae29119567bff81e6757252d90f936e7836e55
gustavomejia-dev <ksks_mejia@hotmail.com>
2023-05-18 14:11:30 -0300

    commit

--- resources/views/livewire/search-zipcode.blade.php
+++ resources/views/livewire/search-zipcode.blade.php
@@ -1,39 +1,42 @@
-<div>
-    <form class = "p-8 bg-blue-100 mx-auto w-1/2 gap-px">
-        <h1>Buscar CEP</h1>
-        <div>
-            <label for="zipcode">CEP</label>
-            <input class = "border" type="text" wire:model.lazy="zipcode"/>
+<div class="">
+    <form class = "rounded-md p-8 bg-blue-100 mx-auto w-1/2 border-solid border-4 border-light-blue-500">
+        <h1 class="text-center">Buscar CEP</h1>
+        <div class="flex flex-col w-1/2">
+            <label for="zipcode ">CEP</label>
+            <input class = "rounded-md border-solid border-4 border-light-blue-500  focus:outline-none" type="text" wire:model.lazy="zipcode"/>
             @error('zipcode')
                 <span class = 'text-red-500' >{{$message}}</span>
             @enderror
         </div>
-        <div>
+        <div class="flex flex-col w-1/2">
             <label for="street">Rua</label>
-            <input  class = "border" type="text" wire:model="street"/>
+            <input  class = "rounded-md border-solid border-4 border-light-blue-500  focus:outline-none" type="text" wire:model="street"/>
         </div>
-        <div>
+        <div class="flex flex-col w-1/2">
             <label for="neighborhood">Bairro</label>
-            <input class = "border" type="text" wire:model="neigborhood"/>
-        </div>
-        <div>
+            <input class = "rounded-md border-solid border-4 border-light-blue-500  focus:outline-none" type="text" wire:model="neighborhood"/>
+        </div class="flex flex-col w-1/2">
+        <div class="flex flex-col w-1/2">
             <label for="city">Cidade</label>
-            <input  class = "border" type="text" wire:model="city"/>
+            <input  class = "rounded-md border-solid border-4 border-light-blue-500  focus:outline-none" type="text" wire:model="city"/>
             @error('city')
                 <span class = 'text-red-500' >{{$message}}</span>
             @enderror
         </div>
-        <div>
+        <div class="flex flex-col w-1/2">
             <label for="state">Estado</label>
-            <input class = "border" type="text" wire:model="state"/>
+            <input class = "rounded-md border-solid border-4 border-light-blue-500  focus:outline-none" type="text" wire:model="state"/>
             @error('state')
                 <span class = 'text-red-500' >{{$message}}</span>
             @enderror
         </div>
         <div>
             <button class = 
-            "px-4 py-2 bg-blue-500 hover:bg-blue-400 text-white rounded-md" 
+            "px-4 py-2 bg-blue-500 hover:bg-blue-400 text-white rounded-md mt-3" 
             type ="button" wire:click="save">Cadastrar</button>
+            <button class = 
+            "px-4 py-2 bg-red-500 hover:bg-red-400 text-white rounded-md mt-3" 
+            type ="button" wire:click="">Resetar</button>
         </div>
         
 
