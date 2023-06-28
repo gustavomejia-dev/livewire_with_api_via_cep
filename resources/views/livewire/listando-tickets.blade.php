e6281fecf3e94bb603655bdc7b81e00d1bbd2d06
gustavomejia-dev <ksks_mejia@hotmail.com>
2023-06-28 17:46:26 -0300

    Modal de alterar/excluir tickets dinamica

--- resources/views/livewire/listando-tickets.blade.php
+++ resources/views/livewire/listando-tickets.blade.php
@@ -12,7 +12,7 @@
             <h1>Funções Rápidas: </h1>   
                 <div class="flex flex-col">
                     <label for="countries" class="block mb-2 text-sm text-center font-medium text-gray-900 dark:text-white">Atribuição em Massa</label>
-                    <select onchange = alterarTickets()  wire:model.defer = "changeTechnical" id="technicals" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <select x-on:change="excluirSim = false" onchange = alterarTickets()  wire:model.defer = "changeTechnical" id="technicals" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option value="*" selected>Selecione o Técnico</option>
                             @foreach ($this->technicals as $technical)
                                 <option value="{{$technical->id}}">{{ $technical->nome }}</option>
@@ -25,7 +25,7 @@
                 </div>
                 <div class="flex flex-col">
                     <label for="countries" class="block mb-2 text-sm text-center font-medium text-gray-900 dark:text-white">Atribuição em Massa</label>
-                    <select id = "status" onchange=alterarTickets() wire:model.defer = "changeStatus" class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
+                    <select id = "status" x-on:change="excluirSim = false" onchange=alterarTickets() wire:model.defer = "changeStatus" class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
                         <option value="*" selected>Todos</option>
                         <option value="R">Resolvido</option>
                         <option value="P">Pendente</option>
